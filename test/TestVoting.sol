2f520d081f85134eedd3e66afec40735d306e97e
Deacon <deacondp@gmail.com>
2018-04-18 14:25:30 +0200

    our session from the other day

--- test/TestVoting.sol
+++ test/TestVoting.sol
@@ -1,18 +1,40 @@
 pragma solidity ^0.4.17;
+//
 import "truffle/Assert.sol";
+
+//fresh instance of a contract being deployed to the BC
+//avoids overwriting or wasting space
 import "truffle/DeployedAddresses.sol";
+
+//contract import
 import "../contracts/Voting.sol";
 
+//test the voting .sol contract
 contract TestVoting{
+
+//instantiating a test contract
 Voting voting = Voting(DeployedAddresses.Voting());
 
-	bytes32 public assignedCandidate;
 
 	function testGetCandidateNames() public {
-	bytes32[] memory newList;
+//Trev testing constructor issue
+	bytes32 public assignedCandidate;
 
+	bytes32[5] public newList;
+//dale's attemt to force values into the array
+	/*bytes32[0] = "test1";
+		bytes32[1] = "test2";
+			bytes32[2] = "test3";
+				bytes32[3] = "test4";
+					bytes32[4] = "test5";
+						bytes32[5] = "test6";
+		*/
+		
 	voting.getCandidateNames(newList);
 
+	
+	function testGetCandidateNames() public {
+	
 	assignedCandidate= voting.candidateList(1);
 	bytes32 originalCanddate = newList[1];
 
